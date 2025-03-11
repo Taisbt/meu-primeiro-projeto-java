5da64829661c695300ed502f5216f4edcc8a497b
Taisbt <taisbatista799@gmail.com>
2025-03-11 16:04:06 +0000

    Correção de texto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bom dia! Aceita um cafezinho??");
+        System.out.println("Bom dia! Aceita um cafezinho?");
     }
 }
